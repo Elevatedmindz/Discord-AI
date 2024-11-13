1141f12ba5a5e72091bbe69e93f5cf89696c736e
Elevatedmindz <carldavidweb@gmail.com>
2024-11-13 17:55:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv('CHIMIRA_GPT_KEY')
 openai.api_base = "https://chimeragpt.adventblocks.cc/v1"
 def printurl():
     # response = openai.Image.create(
-    #     prompt="a cute anime girl doing",
+    #     prompt="a cute anime girl working out",
     #     n=1,
     #     size="256x256"
     # )
